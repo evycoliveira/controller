fd0ba8c5ad450f2d8b9f4627935c9ae73d0889c9
evycoliveira <evelyn.oliveira@mercadolivre.com>
2022-07-05 21:36:14 -0300

    Rename the class * Add POST request for sending user data

--- src/main/java/com/itbootcamp/vehicle/controller/Requests.java
+++ src/main/java/com/itbootcamp/vehicle/controller/Requests.java
@@ -1,10 +1,13 @@
 package com.itbootcamp.vehicle.controller;
 
+import com.itbootcamp.vehicle.dto.UserDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController // Indica que a classe é um controller Rest
 @RequestMapping("/user") // Indica que "/user" é usado para acionar esse controller
-public class Hello {
+public class Requests {
 
     @GetMapping ("/ola/{nome}/{id}")// Método acionado por uma requisição GET
     public String digaHello(@PathVariable String nome, @PathVariable int id) {
@@ -14,5 +17,14 @@ public class Hello {
     @GetMapping ("/ola")// Método acionado por uma requisição GET
     public String digaHelloII(@RequestParam String nome, @RequestParam int id) {
         return "Olá " + nome + " !";
+    }
+
+    @PostMapping
+    public ResponseEntity<User> getUser (@RequestBody User user) {
+        if(user.getId() == 123) {
+            UserDto userDto = new UserDto(user);
+            return new ResponseEntity(userDto, HttpStatus.OK);
+        }
+        return new ResponseEntity(null, HttpStatus.BAD_REQUEST);
     }
 }
